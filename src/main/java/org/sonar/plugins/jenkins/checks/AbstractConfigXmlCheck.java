8b2f41435069d65f6b0bea6b508b300d21b7c668
David Hinske <david.hinske@gmx.net>
2016-06-28 23:12:31 +0200

    renaming

--- src/main/java/org/sonar/plugins/jenkins/checks/AbstractConfigXmlCheck.java
+++ src/main/java/org/sonar/plugins/jenkins/checks/AbstractConfigXmlCheck.java
@@ -7,14 +7,14 @@ import org.sonar.plugins.jenkins.config.types.ConfigXml;
 public abstract class AbstractConfigXmlCheck {
 
 	private RuleKey ruleKey;
-	private ConfigXml JobConfigSource;
+	private ConfigXml jobConfigSource;
 
 	protected final void createViolation(Integer linePosition, String message) {
-		getJobConfigSource().addViolation(new JobConfigIssue(ruleKey, linePosition, message));
+		jobConfigSource.addViolation(new JobConfigIssue(ruleKey, linePosition, message));
 	}
 
 	protected ConfigXml getJobConfigSource() {
-		return JobConfigSource;
+		return jobConfigSource;
 	}
 
 	public final void setRuleKey(RuleKey ruleKey) {
@@ -26,7 +26,7 @@ public abstract class AbstractConfigXmlCheck {
 	}
 
 	protected void setJobConfigSource(ConfigXml JobConfigSource) {
-		this.JobConfigSource = JobConfigSource;
+		this.jobConfigSource = JobConfigSource;
 	}
 
 	public abstract void validate(ConfigXml JobConfigSource);
